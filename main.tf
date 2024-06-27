c92dccce0bd002d4f85f819b4984db3fda9eb9f8
Epredator <trojnaradam@gmail.com>
2024-06-27 22:52:01 +0200

    add 2 virtual networks

--- main.tf
+++ main.tf
@@ -11,7 +11,22 @@ terraform {
 provider "azurerm" {
   features {}
 }
-resource "azurerm_resource_group" "hello_word" {
-  name     = "Hello_Terraform_RG"
-  location = "westus"
+resource "azurerm_resource_group" "lab1" {
+  name     = " etroya_RG"
+  location = "eastus"
+}
+
+resource "azurerm_virtual_network" "main_vnet" {
+ name = "Terraform_VNET"
+ location = azurerm_resource_group.main_rg.location
+resource_group_name = azurerm_resource_group.main_rg.name 
+address_space = ["10.1.0.0/16"]
+subnet {
+name = "dev_subnet"
+address_prefix = "10.1.1.0/24"
+ }
+subnet {
+name = "tst_subnet"
+address_prefix = "10.1.2.0/24"
+ }
 }
