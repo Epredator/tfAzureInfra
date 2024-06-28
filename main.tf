dd36fd7774a6c2b99e5ca1a975bb5fa2ef4cc6e5
Epredator <trojnaradam@gmail.com>
2024-06-28 22:47:26 +0200

    add storage account

--- main.tf
+++ main.tf
@@ -14,7 +14,21 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "main_rg" {
   name     = "etroya_RG"
-  location = "eastus"
+  location = "West Europe"
+}
+
+
+resource "azurerm_resource_group" "app_data_rg" {
+  name     = "App-Data-RG"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "storage_accoung_rg" {
+  name                     = "appdata2234392742894"
+  resource_group_name      = azurerm_resource_group.app_data_rg.name
+  location                 = azurerm_resource_group.app_data_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
 resource "azurerm_virtual_network" "main_vnet" {
