a62e95dbe67a25d62d663e31ae893d95038e46c3
Epredator <trojnaradam@gmail.com>
2024-06-28 21:52:30 +0200

    update 2 subnets

--- main.tf
+++ main.tf
@@ -11,22 +11,25 @@ terraform {
 provider "azurerm" {
   features {}
 }
-resource "azurerm_resource_group" "lab1" {
-  name     = " etroya_RG"
+
+resource "azurerm_resource_group" "main_rg" {
+  name     = "etroya_RG"
   location = "eastus"
 }
 
 resource "azurerm_virtual_network" "main_vnet" {
- name = "Terraform_VNET"
- location = azurerm_resource_group.main_rg.location
-resource_group_name = azurerm_resource_group.main_rg.name 
-address_space = ["10.1.0.0/16"]
-subnet {
-name = "dev_subnet"
-address_prefix = "10.1.1.0/24"
- }
-subnet {
-name = "tst_subnet"
-address_prefix = "10.1.2.0/24"
- }
+  name                = "Terraform_VNET"
+  location            = azurerm_resource_group.main_rg.location
+  resource_group_name = azurerm_resource_group.main_rg.name
+  address_space       = ["10.1.0.0/16"]
+
+  subnet {
+    name           = "dev_subnet"
+    address_prefix = "10.1.1.0/24"
+  }
+
+  subnet {
+    name           = "test_subnet"
+    address_prefix = "10.1.2.0/24"
+  }
 }
