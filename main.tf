7efe73ae30e91c4ed788b011a42435e4132b7322
Epredator <trojnaradam@gmail.com>
2024-06-30 00:18:18 +0200

    add virtual machine and net interface

--- main.tf
+++ main.tf
@@ -47,3 +47,70 @@ resource "azurerm_virtual_network" "main_vnet" {
     address_prefix = "10.1.2.0/24"
   }
 }
+
+
+resource "azurerm_public_ip" "dev01vm_pub_ip" {
+name = "dev01vm_ip"
+resource_group_name = azurerm_resource_group.main_rg.name
+ location = azurerm_resource_group.main_rg.location
+ allocation_method = "Dynamic"
+}
+
+resource "azurerm_network_interface" "dev01vm_nic" {
+ name = "dev01vm-nic"
+ location = azurerm_resource_group.main_rg.location
+resource_group_name = azurerm_resource_group.main_rg.name
+ip_configuration {
+ name = "internal"
+ subnet_id = azurerm_virtual_network.main_vnet.subnet.*.id[0]
+private_ip_address_allocation = "Dynamic"
+public_ip_address_id = azurerm_public_ip.dev01vm_pub_ip.id
+ }
+}
+
+resource "azurerm_network_security_group" "nsg_ssh" {
+ name = "dev01vm-nsg"
+ location = azurerm_resource_group.main_rg.location
+resource_group_name = azurerm_resource_group.main_rg.name
+security_rule {
+ name = "SSH"
+ priority = 300
+ direction = "Inbound"
+ access = "Allow"
+ protocol = "Tcp"
+ source_port_range = "*"
+ destination_port_range = "22"
+ source_address_prefix = ""
+destination_address_prefix = "*"
+ }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_association" {
+network_interface_id = azurerm_network_interface.dev01vm_nic.id
+network_security_group_id = azurerm_network_security_group.nsg_ssh.id
+}
+
+resource "azurerm_linux_virtual_machine" "dev01vm" {
+name = "dev01vm"
+resource_group_name = azurerm_resource_group.main_rg.name
+ location = azurerm_resource_group.main_rg.location
+ size = "Standard_B2s"
+network_interface_ids = [
+azurerm_network_interface.dev01vm_nic.id,
+ ]
+admin_username = "azureuser"
+admin_ssh_key {
+username = "azureuser"
+public_key = file("./.ssh/id_rsa.pub")
+ }
+os_disk {
+caching = "ReadWrite"
+storage_account_type = "Standard_LRS"
+ }
+source_image_reference {
+publisher = "canonical"
+ offer = "0001-com-ubuntu-server-jammy"
+ sku = "22_04-lts-gen2"
+ version = "latest"
+ }
+}
